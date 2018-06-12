d5613acb4e1d11bd9921f07aabafcf6c58f83483
Sarkis Varozian <svarozian@gmail.com>
2018-06-12 16:15:13 -0700

    Init (#1)
    * initial commit
    
    * add codebuild and codepipeline assets
    
    * add variables.tf
    
    * add caller identity and region
    
    * remove buildspec and fix labels
    
    * fixes from CR
    
    * concat instead of overriding attributes
    
    * concat attributes

---
+++ main.tf
@@ -0,0 +1,249 @@
+module "codepipeline_label" {
+  source     = "github.com/cloudposse/terraform-terraform-label.git?ref=0.1.2"
+  attributes = ["${compact(concat(var.attributes, list("codepipeline")))}"]
+  delimiter  = "${var.delimiter}"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  tags       = "${var.tags}"
+}
+
+resource "aws_s3_bucket" "default" {
+  bucket = "${module.codepipeline_label.id}"
+  acl    = "private"
+  tags   = "${module.codepipeline_label.tags}"
+}
+
+module "codepipeline_assume_label" {
+  source     = "github.com/cloudposse/terraform-terraform-label.git?ref=0.1.2"
+  attributes = ["${compact(concat(var.attributes, list("codepipeline", "assume")))}"]
+  delimiter  = "${var.delimiter}"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  tags       = "${var.tags}"
+}
+
+resource "aws_iam_role" "default" {
+  name               = "${module.codepipeline_assume_label.id}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume.json}"
+}
+
+data "aws_iam_policy_document" "assume" {
+  statement {
+    sid = ""
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = "${aws_iam_role.default.id}"
+  policy_arn = "${aws_iam_policy.default.arn}"
+}
+
+resource "aws_iam_policy" "default" {
+  name   = "${module.codepipeline_label.id}"
+  policy = "${data.aws_iam_policy_document.default.json}"
+}
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    sid = ""
+
+    actions = [
+      "ec2:*",
+      "elasticloadbalancing:*",
+      "autoscaling:*",
+      "cloudwatch:*",
+      "s3:*",
+      "sns:*",
+      "cloudformation:*",
+      "rds:*",
+      "sqs:*",
+      "ecs:*",
+      "iam:PassRole",
+    ]
+
+    resources = ["*"]
+    effect    = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3" {
+  role       = "${aws_iam_role.default.id}"
+  policy_arn = "${aws_iam_policy.s3.arn}"
+}
+
+module "codepipeline_s3_policy_label" {
+  source     = "github.com/cloudposse/terraform-terraform-label.git?ref=0.1.2"
+  attributes = ["${compact(concat(var.attributes, list("codepipeline", "s3")))}"]
+  delimiter  = "${var.delimiter}"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  tags       = "${var.tags}"
+}
+
+resource "aws_iam_policy" "s3" {
+  name   = "${module.codepipeline_s3_policy_label.id}"
+  policy = "${data.aws_iam_policy_document.s3.json}"
+}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid = ""
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.default.arn}",
+      "${aws_s3_bucket.default.arn}/*",
+    ]
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild" {
+  role       = "${aws_iam_role.default.id}"
+  policy_arn = "${aws_iam_policy.codebuild.arn}"
+}
+
+module "codebuild_label" {
+  source     = "github.com/cloudposse/terraform-terraform-label.git?ref=0.1.2"
+  attributes = ["${compact(concat(var.attributes, list("codebuild")))}"]
+  delimiter  = "${var.delimiter}"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  tags       = "${var.tags}"
+}
+
+resource "aws_iam_policy" "codebuild" {
+  name   = "${module.codebuild_label.id}"
+  policy = "${data.aws_iam_policy_document.codebuild.json}"
+}
+
+data "aws_iam_policy_document" "codebuild" {
+  statement {
+    sid = ""
+
+    actions = [
+      "codebuild:*",
+    ]
+
+    resources = ["${module.build.project_id}"]
+    effect    = "Allow"
+  }
+}
+
+module "build" {
+  source             = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=tags/0.7.1"
+  namespace          = "${var.namespace}"
+  name               = "${var.name}"
+  stage              = "${var.stage}"
+  build_image        = "${var.build_image}"
+  build_compute_type = "${var.build_compute_type}"
+  buildspec          = "${var.buildspec}"
+  delimiter          = "${var.delimiter}"
+  attributes         = "${concat(var.attributes, list("build"))}"
+  tags               = "${var.tags}"
+  privileged_mode    = "${var.privileged_mode}"
+  aws_region         = "${signum(length(var.aws_region)) == 1 ? var.aws_region : data.aws_region.default.name}"
+  aws_account_id     = "${signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id}"
+  image_repo_name    = "${var.image_repo_name}"
+  image_tag          = "${var.image_tag}"
+  github_token       = "${var.github_oauth_token}"
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_s3" {
+  role       = "${module.build.role_arn}"
+  policy_arn = "${aws_iam_policy.s3.arn}"
+}
+
+resource "aws_codepipeline" "source_build_deploy" {
+  name     = "${module.codepipeline_label.id}"
+  role_arn = "${aws_iam_role.default.arn}"
+
+  artifact_store {
+    location = "${aws_s3_bucket.default.bucket}"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["code"]
+
+      configuration {
+        OAuthToken           = "${var.github_oauth_token}"
+        Owner                = "${var.repo_owner}"
+        Repo                 = "${var.repo_name}"
+        Branch               = "${var.branch}"
+        PollForSourceChanges = "${var.poll_source_changes}"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name     = "Build"
+      category = "Build"
+      owner    = "AWS"
+      provider = "CodeBuild"
+      version  = "1"
+
+      input_artifacts  = ["code"]
+      output_artifacts = ["task"]
+
+      configuration {
+        ProjectName = "${module.build.project_name}"
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ECS"
+      input_artifacts = ["task"]
+      version         = "1"
+
+      configuration {
+        ClusterName = "${var.ecs_cluster_name}"
+        ServiceName = "${var.service_name}"
+      }
+    }
+  }
+}
+
+data "aws_caller_identity" "default" {}
+
+data "aws_region" "default" {}
---
+++ variables.tf
@@ -0,0 +1,115 @@
+variable "namespace" {
+  default     = "global"
+  description = "Namespace, which could be your organization name, e.g. 'cp' or 'cloudposse'"
+}
+
+variable "stage" {
+  default     = "default"
+  description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
+}
+
+variable "name" {
+  default     = "app"
+  description = "Solution name, e.g. 'app' or 'jenkins'"
+}
+
+variable "enabled" {
+  default     = "true"
+  description = "Enable ``CodePipeline`` creation"
+}
+
+variable "ecs_cluster_name" {
+  type        = "string"
+  description = "ECS Cluster Name"
+}
+
+variable "service_name" {
+  type        = "string"
+  description = "ECS Service Name"
+}
+
+variable "github_oauth_token" {
+  description = "GitHub Oauth Token with permissions to access private repositories"
+}
+
+variable "repo_owner" {
+  description = "GitHub Organization or Username."
+}
+
+variable "repo_name" {
+  description = "GitHub repository name of the application to be built and deployed to ECS."
+}
+
+variable "branch" {
+  description = "Branch of the GitHub repository, _e.g._ ``master``"
+}
+
+variable "build_image" {
+  default     = "aws/codebuild/docker:17.09.0"
+  description = "Docker image for build environment, _e.g._ `aws/codebuild/docker:docker:17.09.0`"
+}
+
+variable "build_compute_type" {
+  default     = "BUILD_GENERAL1_SMALL"
+  description = "`CodeBuild` instance size. Possible values are: `BUILD_GENERAL1_SMALL` `BUILD_GENERAL1_MEDIUM` `BUILD_GENERAL1_LARGE`"
+}
+
+variable "buildspec" {
+  default     = ""
+  description = "Declaration to use for building the project. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html)"
+}
+
+# https://www.terraform.io/docs/configuration/variables.html
+# It is recommended you avoid using boolean values and use explicit strings
+variable "poll_source_changes" {
+  type        = "string"
+  default     = "true"
+  description = "Periodically check the location of your source content and run the pipeline if changes are detected"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit', 'XYZ')`"
+}
+
+variable "privileged_mode" {
+  default     = "false"
+  description = "If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
+}
+
+variable "aws_region" {
+  type        = "string"
+  default     = ""
+  description = "AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
+
+variable "aws_account_id" {
+  type        = "string"
+  default     = ""
+  description = "AWS Account ID. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
+
+variable "image_repo_name" {
+  type        = "string"
+  default     = "UNSET"
+  description = "ECR repository name to store the Docker image built by this module. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
+
+variable "image_tag" {
+  type        = "string"
+  default     = "latest"
+  description = "Docker image tag in the ECR repository, e.g. 'latest'. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
+}
