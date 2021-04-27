87fb128e1c939d1798947f91a41794a164a5aa66
Joe Niland <joe@originalmind.com.au>
2021-04-27 13:10:38 +1000

    [BREAKING CHANGE] support type attribute for CodeBuild environment vars (#77)
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -131,10 +131,11 @@ variable "environment_variables" {
     {
       name  = string
       value = string
+      type  = string
   }))
 
   default     = []
-  description = "A list of maps, that contain both the key 'name' and the key 'value' to be used as additional environment variables for the build"
+  description = "A list of maps, that contain the keys 'name', 'value', and 'type' to be used as additional environment variables for the build. Valid types are 'PLAINTEXT', 'PARAMETER_STORE', or 'SECRETS_MANAGER'"
 }
 
 variable "webhook_enabled" {
