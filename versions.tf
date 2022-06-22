7d25dfa8fdd16f60cf855e3de9a379f89e52de3a
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-22 00:11:35 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -6,17 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 2.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.1"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
   }
 }
